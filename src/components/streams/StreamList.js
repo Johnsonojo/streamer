412eff60808f31b5d558be9460d14df026e50ec8
Johnson Ojo <johnsonojo89@gmail.com>
2019-06-24 17:46:03 +0100

    fetch all streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { getStreams } from "../../actions";
 
-const StreamList = () => {
-  return (
-    <div>
-      <h1>StreamList</h1>
-    </div>
-  );
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.getStreams();
+  }
+
+  renderAdminButton(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
+  renderCreateButton() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          {this.renderAdminButton(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">{stream.title}</div>
+          <div className="description">{stream.description}</div>
+        </div>
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreateButton()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
-export default StreamList;
+export default connect(
+  mapStateToProps,
+  { getStreams }
+)(StreamList);

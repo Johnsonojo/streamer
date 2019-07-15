c0b06c7c9568fd89b4e794848ac94690e4029b0d
Johnson Ojo <32887459+Johnsonojo@users.noreply.github.com>
2019-07-15 12:16:57 +0100

    add functionslity fo showing a stream (#14)

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getStream } from "../../actions";
 
-const StreamShow = () => {
-  return (
-    <div>
-      <h1>StreamShow</h1>
-    </div>
-  );
+class StreamShow extends Component {
+  componentDidMount() {
+    this.props.getStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    const { title, description } = this.props.stream;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h4>{description}</h4>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
 };
 
-export default StreamShow;
+export default connect(
+  mapStateToProps,
+  { getStream }
+)(StreamShow);
